f7dba11b26caacb76defd3f163909f807bc97b98
sonovadevs <95682145+sonovadevs@users.noreply.github.com>
2021-12-17 14:02:15 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,240 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import { * as test1} from '@hookform/resolvers';
+import { * as test2} from '@react-native-async-storage/async-storage';
+import { * as test3} from '@react-native-community/netinfo';
+import { * as test4} from '@react-navigation/drawer';
+import { * as test5} from '@react-navigation/native';
+import { * as test6} from '@react-navigation/stack';
+import { * as test7} from '@types/react-native-snap-carousel';
+import { * as test8} from '@types/react-native-vector-icons';
+import { * as test9} from 'dotenv';
+import { * as test10} from 'graphql';
+import { * as test11} from 'graphql-request';
+import { * as test12} from 'i18next';
+import { * as test13} from 'react-hook-form';
+import { * as test14} from 'react-i18next';
+import { * as test15} from 'react-native';
+import { * as test16} from 'react-native-blob-util';
+import { * as test17} from 'react-native-calendars';
+import { * as test18} from 'react-native-config';
+import { * as test19} from 'react-native-gesture-handler';
+import { * as test20} from 'react-native-haptic-feedback';
+import { * as test21} from 'react-native-keychain';
+import { * as test22} from 'react-native-maps';
+import { * as test23} from 'react-native-permissions';
+import { * as test24} from 'react-native-progress';
+import { * as test25} from 'react-native-reanimated';
+import { * as test26} from 'react-native-redash';
+import { * as test27} from 'react-native-screens';
+import { * as test28} from 'react-native-splash-screen';
+import { * as test29} from 'react-native-vector-icons';
+import { * as test30} from 'react-redux';
+import { * as test31} from 'redux';
+import { * as test32} from 'styled-components';
+import { * as test33} from 'yup';
+import { * as test34} from '@babel/core';
+import { * as test35} from '@babel/runtime';
+import { * as test36} from '@react-native-community/eslint-config';
+import { * as test37} from '@storybook/addon-actions';
+import { * as test38} from '@storybook/addon-knobs';
+import { * as test39} from '@storybook/addon-links';
+import { * as test40} from '@storybook/addon-storyshots';
+import { * as test41} from '@testing-library/jest-native';
+import { * as test42} from '@types/jest';
+import { * as test43} from '@types/react-native';
+import { * as test44} from '@types/react-redux';
+import { * as test45} from '@types/styled-components';
+import { * as test46} from '@types/styled-components-react-native';
+import { * as test47} from '@typescript-eslint/eslint-plugin';
+import { * as test48} from 'babel-jest';
+import { * as test49} from 'babel-loader';
+import { * as test50} from 'eslint';
+import { * as test51} from 'eslint-config-airbnb-typescript';
+import { * as test52} from 'eslint-config-prettier';
+import { * as test53} from 'eslint-plugin-import';
+import { * as test54} from 'eslint-plugin-jsx-a11y';
+import { * as test55} from 'eslint-plugin-react';
+import { * as test56} from 'jest';
+import { * as test57} from 'prettier';
+import { * as test58} from 'react-native-codegen';
+import { * as test59} from 'react-native-flipper';
+import { * as test60} from 'ts-jest';
+import { * as test61} from 'typescript';
+
+
+
+
+
+New
+1:49
+import { * as test2} from '@hookform/error-message';
+import { * as test3} from '@hookform/resolvers';
+import { * as test4} from '@react-native-community/art';
+import { * as test5} from '@react-native-community/blur';
+import { * as test6} from '@react-native-community/masked-view';
+import { * as test7} from '@react-native-community/netinfo';
+import { * as test8} from '@react-navigation/drawer';
+import { * as test9} from '@react-navigation/native';
+import { * as test10} from '@react-navigation/stack';
+import { * as test11} from '@types/invariant';
+import { * as test12} from '@types/react-native-snap-carousel';
+import { * as test13} from '@types/react-native-vector-icons';
+import { * as test14} from '@types/react-native-version-check';
+import { * as test15} from 'graphql';
+import { * as test16} from 'graphql-request';
+import { * as test17} from 'invariant';
+import { * as test18} from 'jest-fetch-mock';
+import { * as test19} from 'jwt-decode';
+import { * as test20} from 'moment';
+import { * as test21} from 'react-hook-form';
+import { * as test22} from 'react-native-animated-linear-gradient';
+import { * as test23} from 'react-native-app-auth';
+import { * as test24} from 'react-native-blob-util';
+import { * as test25} from 'react-native-bluetooth-state-manager';
+import { * as test26} from 'react-native-calendars';
+import { * as test27} from 'react-native-config';
+import { * as test28} from 'react-native-gesture-handler';
+import { * as test29} from 'react-native-haptic-feedback';
+import { * as test30} from 'react-native-inset-shadow';
+import { * as test31} from 'react-native-keychain';
+import { * as test32} from 'react-native-linear-gradient';
+import { * as test33} from 'react-native-logs';
+import { * as test34} from 'react-native-maps';
+import { * as test35} from 'react-native-modal';
+import { * as test36} from 'react-native-neomorph-shadows';
+import { * as test37} from 'react-native-pdf';
+import { * as test38} from 'react-native-permissions';
+import { * as test39} from 'react-native-progress';
+import { * as test40} from 'react-native-reanimated';
+import { * as test41} from 'react-native-redash';
+import { * as test42} from 'react-native-safe-area-context';
+import { * as test43} from 'react-native-screens';
+import { * as test44} from 'react-native-snap-carousel';
+import { * as test45} from 'react-native-splash-screen';
+import { * as test46} from 'react-native-status-bar-height';
+import { * as test47} from 'react-native-styled-toast';
+import { * as test48} from 'react-native-swipe-gestures';
+import { * as test49} from 'react-native-swiper';
+import { * as test50} from 'react-native-vector-icons';
+import { * as test51} from 'react-native-version-check';
+import { * as test52} from 'react-redux';
+import { * as test53} from 'redux';
+import { * as test54} from 'redux-logger';
+import { * as test55} from 'redux-persist';
+import { * as test56} from 'redux-persist-transform-filter';
+import { * as test57} from 'redux-saga';
+import { * as test58} from 'styled-components';
+import { * as test59} from 'styled-system';
+import { * as test60} from 'typesafe-actions';
+
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.js</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
